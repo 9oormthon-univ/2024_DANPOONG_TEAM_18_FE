009c9a4c4b1a541be6fe4ef77c76bf114dd23d4f
현지현 <qwere4585@naver.com>
2024-11-20 16:24:18 +0900

    refactor: delete backarrow button

--- src/pages/NumberGame.jsx
+++ src/pages/NumberGame.jsx
@@ -1,9 +1,9 @@
 import styled from "styled-components";
-import backArrow from "../assets/back-arrow.svg";
-import Timer from "../components/Timer";
 import { useNavigate } from "react-router-dom";
-import Button from "../components/Button"
 import { useState, useEffect, useContext } from "react";
+
+import Timer from "../components/Timer";
+import Button from "../components/Button"
 import { ScoreContext } from "../App"
 
 const Number = [1,2,3,4,5,6,7,8,9,10,11,12];
@@ -48,9 +48,6 @@ const NumberGame = () => {
 
   return (
     <Wrapper>
-      <Header>
-        <img src={backArrow} alt="back" width={25} height={25} onClick={() => nav(-1)}/>
-      </Header>
       <Title>숫자 순서 게임</Title>
       <Timer isRunning={isRunning} />
       <NumberGrid>
@@ -64,7 +61,7 @@ const NumberGame = () => {
           </NumberItem>
         ))}
       </NumberGrid>
-      <Button text={"나가기"} />
+      <Button text={"나가기"} onClick={() => nav(-1)}/>
     </Wrapper>
   );
 };
@@ -81,17 +78,13 @@ const Wrapper = styled.div`
   }
 `;
 
-const Header = styled.div`
-  width: 100%;
-  margin: 15px 0px;
-`;
-
 const Title = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
     font-weight: 600;
     font-size: 26px;
+    margin-top: 50px;
     margin-bottom: 20px;
 `
 
