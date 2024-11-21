3e7d24b08d7739c66c6576f4e1622b8c5470bfd1
현지현 <qwere4585@naver.com>
2024-11-21 13:50:59 +0900

    feat: change at midnight logic

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,10 +1,31 @@
 import styled from "styled-components";
 import TreeIcon1 from "../assets/tree-icon-1.svg";
-import { useState } from "react";
+import TreeIcon2 from "../assets/tree-icon-2.svg"
+import { useEffect, useState } from "react";
 import Modal from "../components/Modal";
 
 const Welcome = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [day, setDay] = useState(1);
+
+  useEffect(() => {
+    const now = new Date();
+
+    const nextMidnight = new Date();
+    nextMidnight.setHours(24,0,0,0);
+
+    const timeUntilMidnight = nextMidnight - now;
+
+    const midnightTimeout = setTimeout(() => {
+      setDay((prevDay) => prevDay + 1);
+
+      setInterval(() => {
+        setDay((prevDay) => prevDay + 1);
+      }, 24 * 60 * 60 * 1000);
+    }, timeUntilMidnight);
+
+    return () => clearTimeout(midnightTimeout);
+  },[])
 
   const handleClickNextModal = () => {
     setIsModalOpen(true);
@@ -18,10 +39,16 @@ const Welcome = () => {
         <Wrapper onClick={() => handleClickNextModal()}>
           <MainContent>
             <div>환영합니다 :)</div>
-            <div>1일째 이용중이시네요!</div>
+            <div>{day}일째 이용중이시네요!</div>
           </MainContent>
           <div>오늘도 나무를 키워볼까요?</div>
-          <img src={TreeIcon1} alt="tree-icon" width={100} height={160} />
+          {
+            day < 3 ? (
+              <img src={TreeIcon1} alt="tree-icon-1" width={100} height={160} />
+            ) : (
+              <img src={TreeIcon2} alt="tree-icon-2" width={100} height={180} />
+            )
+          }
           <Comment>반가워요!</Comment>
           <TouchText>화면을 터치해주세요</TouchText>
         </Wrapper>
